a1b92bf69abc680cd351f5bad5e535f3a524384e
Aaron Kennedy <aarontkennedy@MacBook-Air.local>
2018-04-04 21:20:05 -0500

    finished game without the timer portion

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,48 @@
 $(document).ready(function () {
 
+    // object to keep track of question information
     function Question(text, correctAnswer, ans2, ans3, ans4) {
         this.questionText = text;
         this.correct = correctAnswer;
         this.answers = [correctAnswer, ans2, ans3, ans4];
         //debugger
         this.imgSrc = correctAnswer;
-        this.imgSrc = this.imgSrc.replace(/\s/g, "");
-        this.imgSrc = this.imgSrc.replace(/\./g, "");
+        this.imgSrc = this.imgSrc.replace(/\s/g, ""); // strip spaces
+        this.imgSrc = this.imgSrc.replace(/\./g, ""); // strip periods
         this.imgSrc = "assets/images/" + this.imgSrc + ".jpg";
     }
 
     Question.prototype.updateQuestionText = function () {
         $("#question h4").text(this.questionText);
-    }
-
+    };
     Question.prototype.updateQuestionImage = function () {
         $("#pictureHint").attr("src", this.imgSrc);
-    }
-
+    };
     Question.prototype.updateAnswers = function () {
-        
+
         shuffleArray(this.answers);
         $("#answer1").text(this.answers[0]);
         $("#answer2").text(this.answers[1]);
         $("#answer3").text(this.answers[2]);
         $("#answer4").text(this.answers[3]);
+    };
+    Question.prototype.print = function () {
+        this.updateQuestionText();
+        this.updateQuestionImage();
+        this.updateAnswers();
+    };
+    Question.prototype.isAnswerCorrect = function (answer) {
+        if (answer == this.correct) {
+            return true;
+        }
+        return false;
     }
 
 
+    // an array to hold all possible questions
     var questionArray = [];
 
+    // initialization of the question array with questions
     questionArray.push(
         new Question("Who is this giant wrestler?", "Big Show", "Andre the Giant", "Sisco", "The Great Khali"));
 
@@ -101,10 +113,6 @@ $(document).ready(function () {
     questionArray.push(
         new Question("Who is this?", "Undertaker", "Kane", "Paul Bearer", "Matanza Cueto"));
 
-    shuffleArray(questionArray);
-    questionArray[0].updateAnswers();
-    questionArray[0].updateQuestionImage();
-    questionArray[0].updateQuestionText();
 
     /**
      * Randomize array element order in-place.
@@ -129,5 +137,112 @@ $(document).ready(function () {
         // the don't return the array, arrays are passed by reference
         // you are already altering the original array
     }
+
+    function GameScore() {
+        this.reset();
+    }
+
+    GameScore.prototype.reset = function () {
+        this.numCorrect = 0;
+        this.numWrong = 0;
+        this.numNotAnswered = 0;
+        return this;
+    };
+    GameScore.prototype.incrementCorrect = function () {
+        return this.numCorrect++;
+    };
+    GameScore.prototype.incrementWrong = function () {
+        return this.numWrong++;
+    };
+    GameScore.prototype.numNotAnswered = function () {
+        return this.numNotAnswered++;
+    };
+    GameScore.prototype.numQuestions = function () {
+        //debugger
+        return this.numNotAnswered + this.numCorrect + this.numWrong;
+    };
+    GameScore.prototype.updateScore = function () {
+        $("#numCorrect").text(this.numCorrect);
+        $("#totalQuestions").text(this.numQuestions());
+    };
+
+    var score = new GameScore;
+
+
+
+    // start button callback function
+    function startButtonCallback () {
+        $("#startButton").hide().off();
+        $("#theQuiz").show();
+        score.reset().updateScore();
+        shuffleArray(questionArray);  // shuffle the array of questions    
+        goToState2DisplayQuestion();
+    }
+
+    // State 1: Wait for the user to click a button to start the game
+    function state1StartGame() {
+        // hide some of the stuff that is needed for showing 
+        $("#theQuiz").hide();
+        $("#startButton").show();
+
+        // wait for them to click the start button
+        $("#startButton").click(startButtonCallback);
+    }
+    state1StartGame();
+
+    // State 2: Display a question
+    function goToState2DisplayQuestion () {
+        // what number question are we?
+        var numOfCurrentQuestion = score.numQuestions();
+
+        if (numOfCurrentQuestion >= 10) {
+            return goToState3EndGame();
+        }
+
+        questionArray[numOfCurrentQuestion].print();
+
+        // listen to the answer list li's to see if they are clicked
+        $("#answers li").click(function (event) {
+            $("#answers li").off();
+            console.log($(this).text());
+            var result = questionArray[numOfCurrentQuestion].isAnswerCorrect($(this).text());
+            if (result) {
+                score.incrementCorrect();
+            }
+            else {
+                score.incrementWrong();
+            }
+            score.updateScore();
+            goToState2DisplayQuestion();
+        });
+    }
+
+    // State 3: Display results
+    function goToState3EndGame () {
+        $("#answer1").text("Correct Answers: "+score.numCorrect);
+        $("#answer2").text("Incorrect Answers: "+score.numWrong);
+        $("#answer3").text("Unanswered: "+score.numWrong);
+        var percent = Math.floor(score.numCorrect*100/score.numQuestions());
+        $("#answer4").text("Percent Correct: "+ percent +"%");
+        
+        if (percent > 90) {
+            $("#pictureHint").attr("src", "assets/images/weSaluteYou.jpg");
+            $("#question h4").text("Fantastic Job!");
+        }
+        else if (percent >= 70) {
+            $("#pictureHint").attr("src", "assets/images/okayJob.jpg");
+            $("#question h4").text("Slightly Above Average");
+        }
+        else {
+            $("#pictureHint").attr("src", "assets/images/ewwTerrible.jpg");
+            $("#question h4").text("You need to start watching the WWE Network.");
+        }
+
+        $("#startButton").text("Try Again!");
+        // wait for them to click the start button
+        $("#startButton").click(startButtonCallback);
+        $("#startButton").show();
+    }
+
 
 });
